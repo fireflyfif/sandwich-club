490d37ddc1029e139edd9dae345585c6be615919
fireflyfif <fif.iva@gmail.com>
2018-02-17 16:24:25 +0100

    parse the json object, create constraint layout

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,45 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "DetailActivity";
+
+
+    private static final String KNOWN_AS = "alsoKnownAs";
+
+    private static final String SANDWICH_ORIGIN = "placeOfOrigin";
+
+    private static final String DESCRIPTION = "description";
+
+    private static final String IMAGE_PATH = "image";
+
+    private static final String INGREDIENTS = "ingredients";
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView mOrigin;
+    private TextView mDescription;
+    private TextView mIngredients;
+    private TextView mAlsoKnownAs;
+
+    private String mOriginString;
+    private String mDescriptionString;
+    private List<String> mIngredientsList;
+    private List<String> mAlsoKnownAsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +49,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mOrigin = findViewById(R.id.origin_tv);
+        mDescription = findViewById(R.id.description_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
+        mAlsoKnownAs = findViewById(R.id.also_known_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -32,6 +64,33 @@ public class DetailActivity extends AppCompatActivity {
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
+        }
+
+        if (intent.hasExtra(SANDWICH_ORIGIN)) {
+            mOriginString = intent.getStringExtra(SANDWICH_ORIGIN);
+            mOrigin.setText(mOriginString);
+        }
+
+        if (intent.hasExtra(DESCRIPTION)) {
+            // Has to be a List<String>
+            mDescriptionString = intent.getStringExtra(DESCRIPTION);
+            mDescription.setText(mDescriptionString);
+        }
+
+        if (intent.hasExtra(KNOWN_AS)) {
+            // Both are working, which is better?
+            int nameIndex = getIntent().getIntExtra(KNOWN_AS, 0);
+            mAlsoKnownAs.append(mAlsoKnownAsList.get(nameIndex));
+
+            // Both are working, which is better?
+//            mAlsoKnownAsList = intent.getStringArrayListExtra(KNOWN_AS);
+//            String value = mAlsoKnownAsList.get(position);
+//            mAlsoKnownAs.setText(value);
+        }
+
+        if (intent.hasExtra(INGREDIENTS)) {
+            int ingredientIndex = intent.getIntExtra(INGREDIENTS, 0);
+            mIngredients.append(mIngredientsList.get(ingredientIndex));
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
@@ -49,6 +108,17 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+        mOrigin.setText(sandwich.getPlaceOfOrigin());
+        mDescription.setText(sandwich.getDescription());
+
+        mAlsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+
+//        String alsoKnownAsString = mAlsoKnownAs.toString();
+//        String[] part =alsoKnownAsString.split("\\s*,\\s*");
+
+
+        mIngredients.setText(sandwich.getIngredients().toString());
+
     }
 
     private void closeOnError() {
@@ -57,6 +127,7 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,11 +1,100 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String LOG_TAG = JsonUtils.class.getSimpleName();
+
+    private static final String NAMES_ARRAY = "sandwich_names";
+
+    private static final String SANDWICH_DETAILS = "sandwich_details";
+
+    private static final String SANDWICH_NAME = "name";
+
+    private static final String SANDWICH_MAIN_NAME = "mainName";
+
+    private static final String KNOWN_AS = "alsoKnownAs";
+
+    private static final String SANDWICH_ORIGIN = "placeOfOrigin";
+
+    private static final String DESCRIPTION = "description";
+
+    private static final String IMAGE_PATH = "image";
+
+    private static final String INGREDIENTS = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) {
 
-        return null;
+        Sandwich sandwich = new Sandwich();
+
+        List<String> alsoKnownAsList = new ArrayList<>();
+        List<String> ingredientsList = new ArrayList<>();
+
+        try {
+
+            JSONObject sandwichJsonObject = new JSONObject(json);
+
+
+            // Extract the current sandwich name
+            JSONObject sandwichObjectName = sandwichJsonObject.getJSONObject(SANDWICH_NAME);
+            // Get the string with the current name
+            String sandwichName = sandwichObjectName.getString(SANDWICH_MAIN_NAME);
+
+            // Extract the current sandwich also known as
+            JSONArray alsoKnownArray = sandwichObjectName.getJSONArray(KNOWN_AS);
+            for (int i = 0; i < alsoKnownArray.length(); i++) {
+                if (alsoKnownArray.length() != 0) {
+
+                    String value = alsoKnownArray.getString(i);
+
+                    String knownAs = String.valueOf(value);
+                    alsoKnownAsList.add(knownAs);
+                }
+            }
+
+            // Extract the current sandwich place of origin
+            String sandwichOrigin = "";
+            if (sandwichJsonObject.has(SANDWICH_ORIGIN)) {
+                sandwichOrigin = sandwichJsonObject.getString(SANDWICH_ORIGIN);
+            }
+
+            // Extract the current sandwich description
+            String sandwichDescription = sandwichJsonObject.getString(DESCRIPTION);
+
+            String sandwichImagePath = sandwichJsonObject.getString(IMAGE_PATH);
+
+            JSONArray ingredientsArray = sandwichJsonObject.getJSONArray(INGREDIENTS);
+            for (int n = 0; n < ingredientsArray.length(); n++) {
+                if (ingredientsArray.length() != 0) {
+                    String ingredient = ingredientsArray.getString(n);
+                    String sandwichIngredient = String.valueOf(ingredient);
+                    ingredientsList.add(sandwichIngredient);
+                }
+            }
+
+            Log.v(LOG_TAG, "Sandwich name: " + sandwichName +
+                    " \nAlso known as: " + alsoKnownAsList + " \nOrigin: " + sandwichOrigin +
+                    " \nDescription: " + sandwichDescription + " \nImage path: " + sandwichImagePath +
+                    " \nIngredients: " + ingredientsList);
+
+            sandwich = new Sandwich(sandwichName, alsoKnownAsList, sandwichOrigin,
+                    sandwichDescription, sandwichImagePath, ingredientsList);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
 }
